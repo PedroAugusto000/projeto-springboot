6a8c00311bbcad589a1a32e6dd036ac4f8f84f19
Pedro Augusto <pedroaugustomoura000@gmail.com>
2025-02-15 23:48:52 -0300

    Aula Entidade User e seu resource

---
+++ src/main/java/com/workshop/springboot/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.workshop.springboot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.workshop.springboot.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(null, "Pedro", "pedro@gmail.com", "999999", "1411");
+		return ResponseEntity.ok().body(u);
+	}
+}
